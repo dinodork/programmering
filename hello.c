e0f20718b1d0afe6fccc3365865d3eb1d22d4027
Martin Hansson <martinhson@gmail.com>
2023-01-08 21:38:55 +0100

    hello.c made a pure Hello World.

--- hello.c
+++ hello.c
@@ -2,11 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#define yywrap(yyscanner) (/*CONSTCOND*/ 1)
-
 int main() {
 
-  printf("Hello world %d!\n", yywrap(1));
-  char apa[3];
-  strcpy(apa, NULL);
+  printf("Hello world %d!\n");
+
 }
